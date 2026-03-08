4efe27d862c5d03cdce8ff1d94f1f629644079af
ttinonin <tinonin935@gmail.com>
2026-03-08 11:44:14 -0300

    Adding ImGUI

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
 #include <raylib.h>
 
+#include <imgui.h>
+#include <rlImGui.h>
+
 int main()
 {
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	InitWindow(800, 450, "Terraria");
 
+	rlImGuiSetup(true);
+
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		ClearBackground(WHITE);
 
+		rlImGuiBegin();
+
 		DrawText("My Window", 190, 200, 20, RED);
+
+		ImGui::Begin("test");
+
+		ImGui::Text("hello");
+		ImGui::Button("Button");
+
+		ImGui::End();
+
+		rlImGuiEnd();
 
 		EndDrawing();
 	}
 
+	rlImGuiShutdown();
 	CloseWindow();
 
 	return 0;
