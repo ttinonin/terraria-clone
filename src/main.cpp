c014c09c4234cdaa1dd3df444e8e51f23c21fce1
ttinonin <tinonin935@gmail.com>
2026-03-08 11:22:22 -0300

    Creating the window

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <raylib.h>
 
 int main()
 {
-	std::cout << "Hello\n";
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(800, 450, "Terraria");
+
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+		ClearBackground(WHITE);
+
+		DrawText("My Window", 190, 200, 20, RED);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
 
 	return 0;
 }
